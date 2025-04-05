67fa94696dbf29f354145805223da5c29f52994c
Muhammad Nabeel Siddiqui <nabeelsiddiqui324@gmail.com>
2025-04-05 17:23:14 +0500

    files upload

---
+++ resources/js/src/router/index.ts
@@ -0,0 +1,57 @@
+import appSetting from '@/app-setting';
+import { useAppStore } from '@/stores/index';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+import HomeView from '../views/index.vue';
+
+const routes: RouteRecordRaw[] = [
+    // dashboard
+    { path: '/', name: 'home', component: HomeView },
+     // authentication
+     {
+        path: '/auth/boxed-signin',
+        name: 'boxed-signin',
+        component: () => import(/* webpackChunkName: "auth-boxed-signin" */ '../views/auth/boxed-signin.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/boxed-signup',
+        name: 'boxed-signup',
+        component: () => import(/* webpackChunkName: "auth-boxed-signup" */ '../views/auth/boxed-signup.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/boxed-lockscreen',
+        name: 'boxed-lockscreen',
+        component: () => import(/* webpackChunkName: "auth-boxed-lockscreen" */ '../views/auth/boxed-lockscreen.vue'),
+        meta: { layout: 'auth' },
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkExactActiveClass: 'active',
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { left: 0, top: 0 };
+        }
+    },
+});
+
+router.beforeEach((to, from, next) => {
+    const store = useAppStore();
+
+    if (to?.meta?.layout == 'auth') {
+        store.setMainLayout('auth');
+    } else {
+        store.setMainLayout('app');
+    }
+    next(true);
+});
+router.afterEach((to, from, next) => {
+    appSetting.changeAnimation();
+});
+export default router;
